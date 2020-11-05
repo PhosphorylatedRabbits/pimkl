69042706945525d5c20b80e83123499a900f10a3
Joris Cadow <joriscadow@gmail.com>
2020-11-05 15:18:26 +0100

    chore: readthedocs mock of pymimkl.

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,9 @@ import pimkl
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extentions= ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme']
+extentions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme']
 # 'sphinx.ext.napoleon'
+autodoc_mock_imports = ["pymimkl"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
